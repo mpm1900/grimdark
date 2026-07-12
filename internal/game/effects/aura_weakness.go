372bbabc1bf2fe228e5d763a25f0b6cdb3507727
mpm1900 <almostmacmiller@gmail.com>
2026-07-12 17:04:18 -0500

    fix aura of weakness

--- internal/game/effects/aura_weakness.go
+++ internal/game/effects/aura_weakness.go
@@ -22,6 +22,18 @@ func auraOfWeakness() game.Effect {
 					a.Stats[game.Melee] = a.Stats[game.Melee] * 0.75
 					return a
 				})
+				effect.Name = "Weakened"
+				effect.CheckSuccess = func(g *game.Game, e game.Effect, ctx game.Context) {
+					targets := g.FindActors(game.CombineFilters(game.ActiveActors, game.NotSourceActor), ctx)
+					for _, target := range targets {
+						log_ctx := game.MakeContextFor(this.Source, target)
+						log_ctx.EffectID = effect.ID
+						g.PushLogMeta(game.NewLog(
+							"$target$ gained $effect$.",
+							game.EffectTermsTarget(target, effect),
+						).Bind(log_ctx))
+					}
+				}
 				mutation := game.AddModifiers(effect.Bind(ctx))
 				this.Push(mutation.Bind(game.NewContext()))
 				return this.Done()
