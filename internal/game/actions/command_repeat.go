b136bcb201cb71f02bd790934917fed716ce2074
mpm1900 <almostmacmiller@gmail.com>
2026-07-31 22:23:20 -0500

    choice locked

--- internal/game/actions/command_repeat.go
+++ internal/game/actions/command_repeat.go
@@ -2,49 +2,14 @@ package actions
 
 import (
 	"grimdark/internal/game"
-	"slices"
+	"grimdark/internal/game/effects"
 
 	"github.com/google/uuid"
 )
 
 func repeat() game.Effect {
-	effect := game.EffectTargets(game.EffectPriorityActions, func(g *game.Game, a game.Actor, ctx game.Context) game.Actor {
-		struggle_ID := game.Struggle().ID
-		for _, action := range a.GetActions() {
-			if a.Meta.LastUsedActionID == uuid.Nil {
-				continue
-			}
-			if action.ID == a.Meta.LastUsedActionID {
-				continue
-			}
-
-			a.UpdateActionState(action.ID, func(as game.ActionState) game.ActionState {
-				as.IsDisabled = action.ID != struggle_ID && !slices.Contains(action.Tags, game.ATSystem)
-				return as
-			})
-		}
-		return a
-	})
-	effect.Name = "Command: Repeat"
-	effect.Description = "This actor must repeat their last used action."
+	effect := effects.ChoiceLocked()
 	effect.Duration = game.P(5)
-	effect.CheckSuccess = game.EffectGainTargetsOnSuccess
-	effect.CheckFailure = func(g *game.Game, e game.Effect, ctx game.Context) {
-		g.PushLogMeta(game.NewLog(
-			"$effect$ failed.",
-			map[string]string{
-				"$effect$": e.Name,
-			},
-		).Bind(game.NewContext()))
-	}
-	effect.Check = func(g *game.Game, ctx game.Context) bool {
-		for _, t := range g.GetTargets(ctx) {
-			if t.Meta.LastUsedActionID == uuid.Nil {
-				return false
-			}
-		}
-		return true
-	}
 
 	return effect
 }
---
+++ internal/game/effects/choice_locked.go
@@ -0,0 +1,49 @@
+package effects
+
+import (
+	"grimdark/internal/game"
+	"slices"
+
+	"github.com/google/uuid"
+)
+
+func ChoiceLocked() game.Effect {
+	effect := game.EffectTargets(game.EffectPriorityActions, func(g *game.Game, a game.Actor, ctx game.Context) game.Actor {
+		struggle_ID := game.Struggle().ID
+		for _, action := range a.GetActions() {
+			if a.Meta.LastUsedActionID == uuid.Nil {
+				continue
+			}
+			if action.ID == a.Meta.LastUsedActionID {
+				continue
+			}
+
+			a.UpdateActionState(action.ID, func(as game.ActionState) game.ActionState {
+				as.IsDisabled = action.ID != struggle_ID && !slices.Contains(action.Tags, game.ATSystem)
+				return as
+			})
+		}
+		return a
+	})
+	effect.Name = "Choice Locked"
+	effect.Description = "This actor must repeat their last used action."
+	effect.CheckSuccess = game.EffectGainTargetsOnSuccess
+	effect.CheckFailure = func(g *game.Game, e game.Effect, ctx game.Context) {
+		g.PushLogMeta(game.NewLog(
+			"$effect$ failed.",
+			map[string]string{
+				"$effect$": e.Name,
+			},
+		).Bind(game.NewContext()))
+	}
+	effect.Check = func(g *game.Game, ctx game.Context) bool {
+		for _, t := range g.GetTargets(ctx) {
+			if t.Meta.LastUsedActionID == uuid.Nil {
+				return false
+			}
+		}
+		return true
+	}
+
+	return effect
+}
